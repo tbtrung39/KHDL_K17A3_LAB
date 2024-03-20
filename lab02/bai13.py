1b0692d2253d2a4b87574cbb643929efa19dc0a6
nvvinh123 <nvvinh.dhkl17a3hn@sv.uneti.edu.vn>
2024-03-20 23:09:02 +0700

    thuc hanh lab02 da hoan thanh

--- lab02/bai13.py
+++ lab02/bai13.py
@@ -1,17 +1,8 @@
-print("\nphần c.")
-n=int(input("nhập số dòng: "))
-print("Ve tam giac sao deu:");
-for i in range(1, n+1):
-    for j in range(1, n+1-i):
-        print("", end = " ");
-    for k in range(1, i+1):
-        print("*", end=" ");
-    print()
-
-print("\nphần a.")
-height = int(input("Nhập chiều cao của tam giác: "))
-for i in range(height):
-    if i == height - 1: 
-        print('*' * (2 * height - 1))
-    else:
-        print(' ' * (height - i - 1) + '*' + ' ' * (2 * i - 1) + ('*' if i > 0 else ''))
+import math
+S = float(input("Nhập mức cường độ âm của nguồn âm (dB): "))
+D = float(input("Nhập khoảng cách từ nguồn âm đến người nghe (m): "))
+I = S - 20 * math.log10(D)
+if I >= 100:
+    print("Người này sẽ nghe thấy âm.")
+else:
+    print("Người này sẽ không nghe thấy âm.")
