44e79cf1ad19773d244704bd536408856d2f49e8
14_HUNG_0148 <148058664+14-Hung-17A3@users.noreply.github.com>
2024-03-21 20:37:55 +0700

    thuc hanh lab02 da hoan thanh

---
+++ lab02/2.7.py
@@ -0,0 +1,11 @@
+diem = float(input("Nhập điểm TK: "))
+if diem >=0 and diem <=3:
+  print("Loại Kém")
+elif diem ==4:
+  print("Loại Yếu")
+elif diem >=5 and diem <=6:
+  print("Loại Trung bình")
+elif diem >=7 and diem <=8:
+  print("Loại Khá")
+elif diem >=9 and diem <=10:
+  print("Loại giỏi")
