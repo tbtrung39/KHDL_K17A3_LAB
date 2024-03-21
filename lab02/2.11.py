74703f1e65745fbde8ca9e403b5336871d9dfee8
47_Việt_0126 <149389047+47-viet-17a3@users.noreply.github.com>
2024-03-21 22:16:41 +0700

    thuc hanh lab02 da hoan thanh

---
+++ lab02/2.11.py
@@ -0,0 +1,74 @@
+month = int(input("Nhập tháng: "))
+day = int(input("Nhập ngày: "))
+if month ==1:
+  day += 1
+  if day ==31:
+    print("Ngày tiếp theo là 1/2")
+  else:
+    print("Ngày tiếp theo là: ", day)
+if month ==2:
+  day += 1
+  if day ==28:
+    print("Ngày tiếp theo là 1/3")
+  else:
+    print("Ngày tiếp theo là: ", day)
+if month ==3:
+  day += 1
+  if day ==31:
+    print("Ngày tiếp theo là 1/4")
+  else:
+    print("Ngày tiếp theo là: ", day)
+if month ==4:
+  day += 1
+  if day ==30:
+    print("Ngày tiếp theo là 1/5")
+  else:
+    print("Ngày tiếp theo là: ", day)
+if month ==5:
+  day += 1
+  if day ==31:
+    print("Ngày tiếp theo là 1/6")
+  else:
+    print("Ngày tiếp theo là: ", day)
+if month ==6:
+  day += 1
+  if day ==30:
+    print("Ngày tiếp theo là 1/7")
+  else:
+    print("Ngày tiếp theo là: ", day)
+if month ==7:
+  day += 1
+  if day ==31:
+    print("Ngày tiếp theo là 1/8")
+  else:
+    print("Ngày tiếp theo là: ", day)
+if month ==8:
+  day += 1
+  if day ==31:
+    print("Ngày tiếp theo là 1/9")
+  else:
+    print("Ngày tiếp theo là: ", day)
+if month ==9:
+  day += 1
+  if day ==30:
+    print("Ngày tiếp theo là 1/10")
+  else:
+    print("Ngày tiếp theo là: ", day)
+if month ==10:
+  day += 1
+  if day ==31:
+    print("Ngày tiếp theo là 1/11")
+  else:
+    print("Ngày tiếp theo là: ", day)
+if month ==11:
+  day += 1
+  if day ==30:
+    print("Ngày tiếp theo là 1/12")
+  else:
+    print("Ngày tiếp theo là: ", day)
+if month ==12:
+  day += 1
+  if day ==31:
+    print("Ngày tiếp theo là 1/1")
+  else:
+    print("Ngày tiếp theo là: ", day)
