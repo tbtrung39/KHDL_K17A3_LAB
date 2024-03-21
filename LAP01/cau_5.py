73fe70a85511f0b6940e9fd37980c9fac48761ff
khoahocdu lieu 17a3 <ptnuoc.dhkl17a3hn@sv.uneti.edu.vn>
2024-03-22 00:44:22 +0700

    thuc hanh lap01 da hoan thanh

---
+++ LAP01/cau_5.py
@@ -0,0 +1,7 @@
+# câu 5: VIẾT CHƯƠNG TRÌNH NHẬP VÀO GIÁ TRỊ A,B,C CỦA 1 PTRINH BẬC 2. 
+# TÌM ĐỈNH CỦA PTRINH BẬC 2 ĐÓ (LÀM TRÒN ĐẾN SỐ THẬP PHÂN THỨ 2)
+a = int(input("nhập giá trị a:"))
+b = int(input("nhập giá trị b:"))
+c = int(input("nhập giá trị c:"))
+dinh_ptbac2= -b/2*a
+print(f"đỉnh của phương trình bậc 2 là:{dinh_ptbac2}")
