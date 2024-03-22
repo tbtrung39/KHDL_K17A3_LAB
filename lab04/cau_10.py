898477ff2fd5528c6187a876e26c69383001abe6
doan hoai nam <doanhoainam09022005123@gmail.com>
2024-03-22 14:27:35 +0700

    thuc hanh lab04 da hoan thanh

--- lab04/cau_10.py
+++ lab04/cau_10.py
@@ -1,33 +1,26 @@
-def convert_number_to_words(number):
-    # Dictionary chứa các từ tương ứng với các chữ số
-    words_dict = {
-        '0': 'không',
-        '1': 'một',
-        '2': 'hai',
-        '3': 'ba',
-        '4': 'bốn',
-        '5': 'năm',
-        '6': 'sáu',
-        '7': 'bảy',
-        '8': 'tám',
-        '9': 'chín'
-    }
+n=float(input('nhập số thập phân: '))
+chuoi=str(n)
+for i in chuoi:
+    if i=="0":
+        print('không ',end='')
+    if i=='1':
+        print('một ',end='')
+    if i=="2":
+        print('hai ',end='')
+    if i=='3':
+        print('ba ',end='')
+    if i=="4":
+        print('bốn ',end='')
+    if i=='5':
+        print('năm ',end='')
+    if i=="6":
+        print('sáu ',end='')
+    if i=='7':
+        print('bẩy ',end='')
+    if i=="8":
+        print('tám ',end='')
+    if i=='9':
+        print('chín ',end='')
+    if i==".":
+        print("chấm ",end='')
     
-    # Chuyển số thành chuỗi và chia thành từng chữ số
-    digits = str(number)
-    
-    # In ra từng chữ số tương ứng
-    for digit in digits:
-        print(words_dict[digit], end=' ')
-
-def main():
-    number = input("Nhập một số thập phân: ")
-    
-    # Kiểm tra xem số nhập vào có phải là một số thập phân hợp lệ không
-    if number.isdigit():
-        convert_number_to_words(number)
-    else:
-        print("Số bạn nhập không hợp lệ!")
-
-if __name__ == "__main__":
-    main()
--- lab04/cau_3.py
+++ lab04/cau_3.py
@@ -1,1 +1,13 @@
-print(10.32%1)
+import math
+
+x = math.radians(float(input("nhập gia trị x:")))
+n = 0
+term = 1
+sum = term
+
+while abs(term) > 1e-4:
+    n += 1
+    term *= -x**2 / ((2*n-1)*(2*n))
+    sum += term
+
+print(f"Giá trị gần đúng của cos({math.degrees(x)}) sử dụng khai triển Taylor là {sum}")
