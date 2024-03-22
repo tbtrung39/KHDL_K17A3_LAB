c25f1bb5369bd8484bf1cb5f700b7c80c1560536
nvvinh123 <nvvinh.dhkl17a3hn@sv.uneti.edu.vn>
2024-03-22 13:53:36 +0700

    thuc hanh lab04 da hoan thanh

---
+++ lab04/bai2.py
@@ -0,0 +1,35 @@
+#S = 1 - 1/2 + 1/3 - 1/4 +...
+n = int(input("Nhập số nguyên dương n: "))
+tong = 0
+i = 1
+while n<1:
+    n=int(input("Giá trị không hợp lệ, vui lòng nhập lại: "))
+while i<=n:
+    if i % 2 ==0:
+        tong -= 1/i
+    else:
+        tong += 1/i
+    i+=1
+print("Tổng của dãy số S là: ",tong)                    
+
+#S = 1/2 + 1/(2*3) + 1/ (3*4) +...
+n = int(input("Nhập vào số nguyên dương n: "))
+tong = 0
+i = 1
+while n<1:
+    n=int(input("Giá trị không hợp lệ, vui lòng nhập lại: "))
+while i<=n:
+    tong += 1/((i)*(i+1))
+    i+=1 
+print("Tổng của dãy số S là: ",tong)       
+
+import math
+n = int(input("Nhập vào số nguyên dương n: "))
+tong = 0
+i = 2
+while n<2:
+    n=int(input("Giá trị không hợp lệ, vui lòng nhập lại: "))
+while i <=n:
+    tong += 1/(math.sqrt(i))
+    i+=1
+print("Tổng của dãy số S là: ",tong)        
