02f093a10ced9e6f684c434c9fd5a46383c81d7d
Khoa hoc du lieu 17a3 <nmhieu.dhkl17a3hn@sv.edu.vn>
2024-03-23 20:10:43 +0700

    thuc hanh lab02 da hoan thanh

--- lab01/bai2.py
+++ lab01/bai2.py
@@ -4,9 +4,9 @@ r = float(input("Nhập bán kính của hình trụ: "))
 import math
 
 dien_tich_xung_quanh = 2*math.pi*r*h
-dien_tich_toan_phan = 2*math.pi*math.pow(r,2) + 2*math.pi*r*h
-the_tich_hinh_tru = math.pi*math.pow(r,2)*h
+dien_tich_toan_phan = 2*math.pi*math.pow(r ,2) + 2*math.pi*r*h
+the_tich_hinh_tru = math.pi*math.pow(r ,2)*h
 
-print("Diện tích xung quanh của hình trụ là: %.2f"%dien_tich_xung_quanh)
-print("Diện tích toàn phần của hình trụ là: %.2f"%dien_tich_toan_phan)
-print("Thể tích của hình trụ là: %.2f"%the_tich_hinh_tru)
+print("Diện tích xung quanh của hình trụ là: %.2f" %dien_tich_xung_quanh)
+print("Diện tích toàn phần của hình trụ là: %.2f" %dien_tich_toan_phan)
+print("Thể tích của hình trụ là: %.2f" %the_tich_hinh_tru)
---
+++ lab02/bai 10.py
@@ -0,0 +1,12 @@
+
+bat_dau = float(input("Nhập giờ bắt đầu: "))
+ket_thuc = float(input("Nhập giờ kết thúc: "))
+if 5 <= bat_dau <= ket_thuc <= 22:
+    gio_thue = ket_thuc - bat_dau
+    if gio_thue <= 3:
+        tien_san = gio_thue * 100000
+    elif gio_thue > 3:
+        tien_san = 3 * 100000 + (gio_thue - 3) * (100000 * 0.75)
+if 11 <= bat_dau <= ket_thuc <= 15:
+    tien_san = tien_san * 0.9
+print("Số tiền phải trả là: ", tien_san)
