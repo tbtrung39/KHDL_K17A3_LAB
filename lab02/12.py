261ba5b775e7d4350bff8d1f23247dcc5b1fb10b
doan hoai nam <doanhoainam09022005123@gmail.com>
2024-03-23 22:59:06 +0700

    thuc hanh lab02 da hoan thanh

---
+++ lab02/12.py
@@ -0,0 +1,12 @@
+a=float(input('nhap a:'))
+b=float(input('nhap b:'))
+c=float(input('nhap c:'))
+d=float(input('nhap d:'))
+r=float(input('nhap r:'))
+kc=((c-a)**2+(d-b)**2)**0.5
+if kc<r:
+    print('diem m nam ben trong duong tron')
+elif kc==r:
+    print('diem m nam tren duong trong')
+else:
+    print('diem m nam ben ngoai duong tron')
