0462cad8aceba1d96398ae5da88cee2824e66f54
10-buiduchiep-17a3 <bdhiep.dhkl17a3hn@sv.uneti.edu.vn>
2024-03-23 15:54:45 +0700

    thuc hanh lab02 da hoan thanh

---
+++ lab2/lab2/3.py
@@ -0,0 +1,17 @@
+day = int(input('nhap so ngay(1->7):\n'))
+if day==1:
+    print(f'{day}:Sunday')
+elif day == 2:
+    print(f'{day}:Monday')
+elif day == 3:
+    print(f'{day}:Tuesday')
+elif day == 4:
+    print(f'{day}:Wednesday')
+elif day == 5:
+    print(f'{day}:Thursday')
+elif day == 6:
+    print(f'{day}:Friday')
+elif day == 7:
+    print(f'{day}:Saturday')
+else:
+    print('nhap sai')
