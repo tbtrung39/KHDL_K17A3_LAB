685700f793e5206a4ec877cc6f46e081243f4435
Le thi hương phan <lthphan.dhkl17a3hn@sv.uneti.edu.vn>
2024-03-24 23:59:43 +0700

    thuc hanh lab02 da hoan thanh

--- lab01/7.py
+++
@@ -1,6 +0,0 @@
-x= float(input('nhap x: '))
-y=float(input('nhap y: '))
-z=float(input('nhap z: '))
-print(f'toa do doi xung qua oxy:x={x},y={y},z={-z}')
-print(f'toa do doi xung qua oxz:x={x},y={-y},z={z}')
-print(f'toa do doi xung qua oyz:x={-x},y={y},z={-z}')
