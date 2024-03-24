ea020e3c4c57364ba25fbe604f7283209d931bfd
Le thi hương phan <lthphan.dhkl17a3hn@sv.uneti.edu.vn>
2024-03-24 23:50:33 +0700

    thuc hanh lab01 da hoan thanh

---
+++ lab01/7.py
@@ -0,0 +1,6 @@
+x= float(input('nhap x: '))
+y=float(input('nhap y: '))
+z=float(input('nhap z: '))
+print(f'toa do doi xung qua oxy:x={x},y={y},z={-z}')
+print(f'toa do doi xung qua oxz:x={x},y={-y},z={z}')
+print(f'toa do doi xung qua oyz:x={-x},y={y},z={-z}')
---
+++ lab01/9.py
@@ -0,0 +1,2 @@
+n=int(input('nhap n: '))
+print(f'xac suot khi tung 3 xuc sac cos it nhat 1 lan ca 3 ra 6 la: {round(((1 -(5/6)**n)**3),2)}')
