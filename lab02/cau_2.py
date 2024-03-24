1e643e401b4a5031eca92a44fb472b0fa4d33cd0
phandoanduchuy <pddhuy.dhkl17a3hn@sv.uneti.edu.vn>
2024-03-25 02:08:32 +0700

    thuc hanh lab02 da hoan thanh

---
+++ lab02/cau_2.py
@@ -0,0 +1,19 @@
+import math
+
+a = float(input("Nhập hệ số a: "))
+b = float(input("Nhập hệ số b: "))
+c = float(input("Nhập hệ số c: "))
+delta = b**2 - 4*a*c
+
+if delta > 0:
+    x1 = (-b + math.sqrt(delta)) / (2*a)
+    x2 = (-b - math.sqrt(delta)) / (2*a)
+    print("Phương trình có hai nghiệm phân biệt:")
+    print("x1 =", x1)
+    print("x2 =", x2)
+elif delta == 0:
+    x = -b / (2*a)
+    print("Phương trình có nghiệm kép:")
+    print("x =", x)
+else:
+    print("Phương trình vô nghiệm")
