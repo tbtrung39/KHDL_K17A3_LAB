7641fd820e92bdfa642b12360b163076cb88a39c
38-THÀNH-0047 <148701669+38-THANH-0047@users.noreply.github.com>
2024-03-29 22:08:22 +0700

    thuc hanh lab01 da hoan thanh

---
+++ lab01/bai7.py
@@ -0,0 +1,6 @@
+Ax, Ay, Az = input("nhap toa do dinh A: ").split()
+Ax, Ay, Az = float(Ax),float(Ay),float(Az)
+
+print(f"diem dx qua mp Oxy là: {Ax},{Ay},{-Az}")
+print(f"diem dx qua mp Oxz là: {Ax},{-Ay},{Az}")
+print(f"diem dx qua mp Oyz là: {-Ax},{Ay},{Az}")
