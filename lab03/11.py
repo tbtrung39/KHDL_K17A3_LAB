22189bd2cc0f2e5277f1d7f2ddf40e2b4080b2c8
Le thi hương phan <lthphan.dhkl17a3hn@sv.uneti.edu.vn>
2024-03-30 13:12:01 +0700

    thuc hanh lab03 da hoan thanh

---
+++ lab03/11.py
@@ -0,0 +1,23 @@
+n=int(input('nhap số hàng: '))
+
+print(" "*(n-1)+'*')
+for i in range(1,n-1):
+    print(" "*(n-i-1)+'*',end='')
+    print(" "*(2*i-1),end='')
+    print("*")
+print('*'*(2*n-1))
+print('hình 1')
+
+print(" "*(n-1)+'*')
+for i in range(1,n-1):
+    print(" "*(n-i-1)+'*',end='')
+    print(" "*(2*i-1),end='')
+    print("*")
+print('* '*(n))
+print('hình 2')
+
+for i in range(n):
+    print(' '*(n-i-1),end='')
+    print('* '*(i+1),end='')
+    print()
+print('hình 3')
