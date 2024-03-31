a5809683b3d264c4bfe31d9f7c42ad770b778fa8
Khoa hoc du lieu 17A3 <vttdung.dhkl17a3hn@sv.uneti.edu.vn>
2024-04-01 00:58:25 +0700

    thuc hanh lab 04 da hoan thanh

---
+++ Lab_04/c4.py
@@ -0,0 +1,7 @@
+tu_so = int(input("Nhập tử số: "))
+mau_so = int(input("Nhập mẫu số: "))
+while mau_so ==0:
+    print("Hãy nhập lại mẫu số")
+    break
+else:
+    print("Phân số bạn chọn là: ", tu_so, "/", mau_so)
