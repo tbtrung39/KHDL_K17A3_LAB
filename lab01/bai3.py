87c4402a204b78532e2a53acfa28597129c2ea8a
38-THÀNH-0047 <148701669+38-THANH-0047@users.noreply.github.com>
2024-03-31 07:46:10 +0700

    thuc hanh lab01 da hoan thanh

---
+++ lab01/bai3.py
@@ -0,0 +1,8 @@
+Ax, Ay = input("nhap toa do vecto A: ").split()
+Ax, Ay = float(Ax),float(Ay)
+
+Bx, By = input("nhap toa do veto B: ").split()
+Bx, By = float(Bx),float(By)
+
+tich_vo_huong = Ax*Bx + Ay*By
+print("tich vo huong cua 2 vecto la: ", tich_vo_huong)
