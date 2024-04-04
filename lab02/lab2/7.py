7a72933ab9bb2403680d83589df58e1529a0ed34
Khoa hoc Du lieu 17A3hn <ptmai.dhkl17a3hn@sv.uneti.vn>
2024-04-05 00:16:11 +0700

    thuc hanh lab2 da hoan thanh

---
+++ lab02/lab2/7.py
@@ -0,0 +1,13 @@
+a = float(input("nhap mot so :"))
+if a == 0 and a < 3.0 : 
+    print("loai kem")
+elif  a < 4.0 :
+    print("loai yeu")
+elif  a < 6.0 :
+    print("loai trung binh")
+elif  a < 8.0 :
+    print("loai kha")
+elif a < 10.0 :
+    print ("loai gioi")
+else:
+    print("khong ton tai")
