3898728d821ab3cf8eef15e1befc04878a684d1c
13- Bùi Huy Hoàng - 0033 <148078872+13-HOANG-0033@users.noreply.github.com>
2024-04-06 21:46:49 +0700

    thuc hanh lab05 da hoan thanh

---
+++ lab05/c8.py
@@ -0,0 +1,11 @@
+text = '''
+Một mai, một cuốc, một cần câu
+Thơ thẩn dầu ai vui thú nào
+Ta dại, ta tìm nơi vắng vẻ
+Người khôn, người đến chốn countdown
+Thu ăn măng trúc, đông ăn giá
+Xuân tắm hồ sen, hạ tắm ao
+Rượu, đến gốc cây, ta sẽ uống
+Nhìn xem phú quý, tựa chiêm bao.'''
+check = text.count("mai")
+print(check)
