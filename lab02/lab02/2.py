a77c9f5219fef6eeb81d1dd84f75667b544bceaf
khoa hoc du lieu 17a3 <ntanh.dhkl17a3hn@sv.uneti.edu.vn>
2024-04-07 00:10:35 +0700

    thuc hanh lab01 da hoan thanh

---
+++ lab02/lab02/2.py
@@ -0,0 +1,20 @@
+#ax^2+bx+c=0
+import math
+a=float(input("Nhập vào a:"))
+b=float(input("Nhập vào b:"))
+c=float(input("Nhập vào c:"))
+delta=b**2 - 4*a*c 
+if a == 0:
+    x=-c/b
+    print(f"Nghiệm của phương trình là : {x}")
+elif delta == 0:
+    x=-b/2*a
+    print(f"Nghiệm của phương trình là {x}")
+elif delta > 0:
+    x1=(-b+math.sqrt(delta))/2*a
+    x2=(-b+math.sqrt(delta))/2*a
+    print(f"Nghiệm của phương trình là : {x1} và {x2}")
+elif delta < 0:
+    print(f"Nghiệm của phương trình là : {-b/2*a}+{math.sqrt(-delta)/2*a}i và {-b/2*a}-{math.sqrt(-delta)/2*a}i")
+else:
+    print("Sai rồi cu")
