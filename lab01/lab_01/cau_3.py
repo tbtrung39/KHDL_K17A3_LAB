c324606a2e26e4a6bd6da19cd1cf95b1dd7461a3
Khoa hoc du lieu 17a3 <ntkhoi.dhkl17a3hn@sv.uneti.edu.vn>
2024-04-07 10:49:06 +0700

    thuc hanh lab01 da hoan thanh

---
+++ lab01/lab_01/cau_3.py
@@ -0,0 +1,8 @@
+# Câu 3: VIẾT CHƯƠNG TRÌNH NHẬP VÀO 2 VECTOR A VÀ B .
+# TÍNH TÍCH VÔ HƯỚNG CỦA 2 VECTOR ĐÓ
+a_1 = int(input("nhập giá trị a_1:"))
+a_2 = int(input("nhập giá trị a_2:"))
+b_1 = int(input("nhập giá trị b_1:"))
+b_2 = int(input("nhập giá trị b_2:"))
+tich_vo_huong = a_1*b_1 + a_2*b_2
+print("tích vô hướng:",tich_vo_huong)
