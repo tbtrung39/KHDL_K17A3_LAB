6e78f47e00a7b2ec76b9773742e20a248bdeb7c7
13- Bùi Huy Hoàng - 0033 <148078872+13-HOANG-0033@users.noreply.github.com>
2024-04-13 21:40:16 +0700

    thuc hanh lab06 da hoan thanh

---
+++ lab06/c7.py
@@ -0,0 +1,18 @@
+lst = [["Mon",73],["Tue",89],["Wed",95],["Thu",103],["Fri",115],["Sat",128],["Sun",120]]
+print(enumerate(lst))
+
+# chọn phần tử thứ 2
+print(lst[2])
+
+# kiểm tra độ dài và  và thêm sublist ngẫu nhiên
+print(len(list))
+mini_list = ["Mon2",82]
+list.insert(0,mini_list)
+print(list)
+
+#  tính tổng sale value
+tong = 0
+for i in range(int(lst)):
+    tong += i
+print(tong)
+
