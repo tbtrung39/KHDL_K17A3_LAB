a753a6028e6e249d75875f0c4871fa4858608b1c
khdl <leanhvuhy@gmail.com>
2024-04-25 08:40:02 +0700

    thuc hanh lab0 da hoan thanh

---
+++ lab08/6.py
@@ -0,0 +1,11 @@
+def tim_bcnn(a,b):    
+    def ucln(a,b):
+        while(b):
+            a,b = b, a % b
+        return a
+    return a * b // ucln(a,b)
+ 
+a = int(input("nhap so a :"))
+b = int(input("nhap so b :"))
+bcnn = tim_bcnn(a,b)
+print("boi chung nho nhat cua a va b la :",bcnn)    
