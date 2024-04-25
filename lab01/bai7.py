170dc0c3717909fb5fcdb0c66cd31c6d51467f56
khdl <leanhvuhy@gmail.com>
2024-04-25 08:18:52 +0700

    thuc hanh lab01 da hoan thanh

---
+++ lab01/bai7.py
@@ -0,0 +1,6 @@
+Ax, Ay, Az = input("nhap toa do dinh A: ").split()
+Ax, Ay, Az = float(Ax),float(Ay),float(Az)
+
+print("diem doi xung qua mat phang oxy la :" ,Ax,Ay,-Az)
+print("diem doi xung qua mat phang oyz la :" ,-Ax,Ay,Az)
+print("diem doi xung qua mat phang oxz la :" ,Ax,-Ay,Az)
