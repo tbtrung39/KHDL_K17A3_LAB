defc3821d4da842a169c25a90f9670b715cc0413
Khoa hoc du lieu 17A3 <ndthang.khdl17a3hn@sv.uneti.edu.vn>
2024-04-28 21:51:23 +0700

    commit

---
+++ lab03/8.py
@@ -0,0 +1,86 @@
+# a
+n = int(input("Nhập số nguyên n :"))
+S4 = 0
+for i in range(0,n + 1):
+    if n <= 0 :
+        break
+    else:
+        S4 += i
+print(f"S4 : {S4}")
+        
+# b
+n = int(input("Nhập số nguyên n :"))
+S5 = 0
+for i in range(0,n + 1):
+    if n <= 0 :
+        break
+    else:
+        S5 += 2 * i + 1
+print(f"S5 : {S5}")
+        
+# c
+# a
+n = int(input("Nhập số nguyên n :"))
+S6 = 0
+for i in range(0,n + 1):
+    if n <= 0 :
+        break
+    else:
+        S6 += i
+print(f"S6 : {S6}")
+        
+     
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+             
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+             
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
