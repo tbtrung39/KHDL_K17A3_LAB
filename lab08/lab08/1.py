c4e1b4ce13436c4a663e779c444f1061d37bc7dd
khoa hoc du lieu 17a3 <ntanh.dhkl17a3hn@sv.uneti.edu.vn>
2024-05-02 20:17:48 +0700

    thuc anh lab08 da hoan thanh

---
+++ lab08/lab08/1.py
@@ -0,0 +1,6 @@
+def in_so_ke_tiep():
+    so = int(input("nhap so :"))
+    print("so ke tiep la :",so + 1)
+in_so_ke_tiep()
+
+    
---
+++ lab08/lab08/7.py
@@ -0,0 +1,12 @@
+def so():
+    a = int(input("nhap so a :"))
+    b = int(input("nhap so b :"))
+    c = int(input("nhap so c :"))
+
+    so_max = max(a,b,c)
+    so_min = min(a,b,c)
+    
+    print("so lon nhat la :",so_max)
+    print("so nho nhat la :",so_min)
+so()
+    
