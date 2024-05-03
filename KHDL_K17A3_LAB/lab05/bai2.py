cd1a4e0f2c720dc2c9eb58b7105cea62e0ee4177
dangsonjr24 <nds,dhkl17a3hn@sv.uneti.edu.vn>
2024-05-03 13:59:28 +0700

    thuc hanh lab05 da hoan thanh

---
+++ KHDL_K17A3_LAB/lab05/bai2.py
@@ -0,0 +1,6 @@
+Str = input("nhập 1 chuỗi kí tự: ")
+dem = 0
+for i in Str:
+    if not i.isalpha() and i.isnumeric():
+        dem += 1
+print(f"có {dem} ký tụe kp là chữ cái TA và kp là số trong chuỗi vừa nhập")
