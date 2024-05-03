bd002213d68ea53225478ad9c2aa323f278ed681
dangsonjr24 <nds,dhkl17a3hn@sv.uneti.edu.vn>
2024-05-03 14:10:22 +0700

    thuc hanh lab07 da hoan thanh

---
+++ KHDL_K17A3_LAB/lab07/bai6.py
@@ -0,0 +1,16 @@
+n = int(input("Nhập một số tự nhiên: "))
+
+a = 0
+num = 2
+print(n, "số nguyên tố đầu tiên là:")
+
+while a < n:
+    prime = True
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            prime = False
+            break
+    if prime:
+        print(num)
+        a += 1
+    num += 1
