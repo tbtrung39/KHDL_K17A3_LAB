4e18432053f686256e113f2d241d050a6e2ab2e7
08-Dung-17A3 <vttdung.dhkl17a3hn@sv.uneti.edu.vn>
2024-05-05 19:19:17 +0700

    thuc hanh lab04 da hoan thanh

---
+++ lab04/c11.py
@@ -0,0 +1,19 @@
+order = int(input("Bạn muốn gọi đồ uống nào? "))
+while order ==1:
+    print("Bạn đã đặt cafe")
+    break
+while order ==2:
+    print("Bạn đã đặt cam vắt")
+    break
+while order ==3:
+    print("Bạn đã đặt nước ép cà rốt")
+    break
+while order ==4:
+    print("Bạn đã đặt nước lọc")
+    break
+while order ==5:
+    print("Bạn đã đặt nước dừa")
+    break
+while order >5:
+    print("Không hợp lệ")
+    break
