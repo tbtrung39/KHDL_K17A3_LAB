55533c89dc0b50f053099429c00b53650f5d32f9
khdl <leanhvuhy@gmail.com>
2024-05-06 22:00:38 +0700

    thuc hanh lab08 da hoan thanh

---
+++ lab08/2.py
@@ -0,0 +1,14 @@
+def tim_ucln(a,b):
+    while(b):
+        a,b = b, a % b
+        return a
+
+def rut_gon_phan_so(a,b):
+        a = a // ucln
+        b = b // ucln
+        return a,b
+
+a = int(input("nhap a :"))
+b = int(input("nhap b :"))
+ucln = tim_ucln(a,b) 
+print("phan so rut gon la :",a, "/",b)    
