6f182d807e5cb5fe8832fc741fe98fd6605f2ac8
khdl <leanhvuhy@gmail.com>
2024-05-06 21:54:17 +0700

    thuc hanh lab07 da hoan thanh

---
+++ lab07/2.py
@@ -0,0 +1,16 @@
+# Khởi tạo danh sách
+number = []
+
+print("Nhập các số tự nhiên từ bàn phím (nhấn 'q' để kết thúc):")
+
+while True:
+    num = input()
+    if num == 'q':  # Kết thúc khi người dùng nhập 'q'
+        break
+    number.append(int(num))
+
+# Tạo tập hợp A từ danh sách number
+A = set(number)
+
+print("Tập hợp A sau khi nhập các số:", A)
+
