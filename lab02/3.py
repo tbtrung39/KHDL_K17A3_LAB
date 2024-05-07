5918aea3a08a4898b8776dc38ea54362d2ad9437
bao17A3 <148509725+NGUYENQUOCBA0@users.noreply.github.com>
2024-05-07 18:09:55 +0700

    thuc hanh lab02 da hoan thanh

---
+++ lab02/3.py
@@ -0,0 +1,17 @@
+day_number = int(input("Nhập vào một số từ 1 đến 7: "))
+if day_number == 1:
+        print("Thứ bạn đã nhập là: Sunday")
+elif day_number == 2:
+        print("Thứ bạn đã nhập là: Monday")
+elif day_number == 3:
+        print("Thứ bạn đã nhập là: Tuesday")
+elif day_number == 4:
+        print("Thứ bạn đã nhập là: Wednesday")
+elif day_number == 5:
+        print("Thứ bạn đã nhập là: Thursday")
+elif day_number == 6:
+        print("Thứ bạn đã nhập là: Friday")
+elif day_number == 7:
+        print("Thứ bạn đã nhập là: Saturday")
+else:
+        print("Số bạn nhập không hợp lệ. Vui lòng nhập một số từ 1 đến 7.")
