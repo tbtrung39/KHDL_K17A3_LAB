f96a662f3dd7bd8d0a7cc1da5af3c6a8721afa3d
14_HUNG_0148 <148058664+14-Hung-17A3@users.noreply.github.com>
2024-05-11 22:54:04 +0700

    thuc hanh lab09 da hoan thanh

---
+++ lab09/c2.py
@@ -0,0 +1,17 @@
+num1 = int(input("Nhập số thứ 1: "))
+num2 = int(input("Nhập số thứ 2: "))
+if num1 > num2:
+    min = num2
+    max = num1
+else:
+    min = num1
+    max = num2
+
+def the_gcd_of_two_numbers(max,min):
+    if min == 0:
+        return max
+    else:
+        return the_gcd_of_two_numbers(min, max % min)
+result = the_gcd_of_two_numbers(max,min)
+print(result, "là ước chung lớn nhất")
+    
