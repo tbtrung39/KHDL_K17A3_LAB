995c05db088ac2ac746201607f2365f0fa27b869
32-leminhphuong-17a3 <[200~>
2024-05-19 17:39:22 +0700

    thuc hanh lab010 da hoan thanh

---
+++ lab10/package_4/cau4.py
@@ -0,0 +1,6 @@
+import my_equation
+a = int(input("Nhập số: "))
+b = int(input("Nhập số: "))
+c = int(input("Nhập số: "))
+print(my_equation.pt_bac_nhat_mot_an(a,b), "là nghiệm pt bậc nhất 1 ẩn")
+print(my_equation.pt_bac_hai(a,b,c), "là nghiệm pt bậc 2")
