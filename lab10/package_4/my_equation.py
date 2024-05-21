d498925a4b4e8d63c2955a1aa5558bbbadc71f30
myloin147@gmail.com <myloin147@gmail.com>
2024-05-21 20:53:42 +0700

    thuc hanh lab 10 da hoan thanh

---
+++ lab10/package_4/my_equation.py
@@ -0,0 +1,19 @@
+def pt_bac_nhat_mot_an(a,b):
+    if a ==0:
+        print("Vui lòng nhập lại")
+    else:
+        return -b/a
+    
+import math
+def pt_bac_hai(a,b,c):
+    delta = b**2 - 4*a*c
+    if delta <0:
+        print("Pt vô nghiệm")
+    elif delta ==0:
+        return -b / (2*a)
+    else:
+        print("Nghiệm 1: ", (-b + math.sqrt(delta) / (2*a)))
+        print("Nghiệm 2: ", (-b - math.sqrt(delta) / (2*a)))
+
+
+
---
+++ lab10/package_5/c5.py
@@ -0,0 +1,5 @@
+import doicoso1
+number = int(input("Nhập số: "))
+print(doicoso1.convert_number_to_binary(number), "là hệ nhị phân của số bạn nhập")
+print(doicoso1.convert_number_to_octal(number), "là hệ bát phân của số bạn nhập")
+print(doicoso1.convert_number_to_hexadecimal(number), "là hệ thập lục phân của số bạn nhập")
