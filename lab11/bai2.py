1a9b28278155f5f9e7a6b4216dd6178d7cea28e3
32-leminhphuong-17a3 <[200~>
2024-06-02 15:38:51 +0700

    thuc hanh lab11 da hoan thanh

---
+++ lab11/bai2.py
@@ -0,0 +1,10 @@
+
+with open("lab11/Inp.txt",mode='r') as file:
+    data = file.readline()
+
+numbers = list(data.split())
+numbers.sort(reverse=False)
+
+with open("lab11/out.dat",mode='w') as file:
+    for num in numbers:
+        file.write(num)
