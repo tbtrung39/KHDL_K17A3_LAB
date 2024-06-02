39ed5c9e0285bc549b86e1e97272c61c8814b832
KHOA HOC DU LIEU 17A3 <dckien.dhkl17a3hn@sv.uneti.edu.vn>
2024-06-02 10:51:15 +0700

    thuc hanh lab12 da hoan thanh

---
+++ lab12/bai tap lab12/bai7.py
@@ -0,0 +1,20 @@
+from datetime import datetime, timedelta
+
+def next_day(day, month, year):
+    input_date = datetime(year, month, day)
+    
+    # Tính toán ngày kế tiếp bằng cách cộng thêm 1 ngày
+    next_date = input_date + timedelta(days=1)
+    
+    # Trả về ngày kế tiếp dưới dạng ngày, tháng, năm
+    return next_date.day, next_date.month, next_date.year
+
+
+day = int(input("Nhập ngày: "))
+month = int(input("Nhập tháng: "))
+year = int(input("Nhập năm: "))
+
+# Tìm ngày kế tiếp
+day_next, month_next, year_next = next_day(day, month, year)
+
+print(f"Ngày kế tiếp là: {day_next}/{month_next}/{year_next}")
