335cc45ac8184de3c18c231061eb6ab467d7690b
phandoanduchuy <pddhuy.dhkl17a3hn@sv.uneti.edu.vn>
2024-06-02 08:29:18 +0700

    thuc hanh lab07 da hoan thanh

---
+++ lab07/bai8.py
@@ -0,0 +1,11 @@
+a = set()
+while True:
+    elm = input("nhap cac phan tu (enter de ket thuc) :")
+    if elm == '':
+        break
+    a.add(elm)
+so_nguyen = sum(n.isdigit() for n in a)
+so_thuc = sum(',' in n and n.replace(',','',1).isdigit() for n in a)
+str = len(a) - so_nguyen - so_thuc
+print(f'so luong phan tu nguyen trong tap hop la :{so_nguyen}')
+print(f'so luong phan tu cuoi trong chuoi ki tu trong tap hop : {so_thuc}')
