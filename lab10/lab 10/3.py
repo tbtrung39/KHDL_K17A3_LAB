a3b4ac5fb14499624f45a75eb7183bc42e4d6045
PhuongTkao <btpthao.dhkl17a3hn@sv.uneti.edu.vn>
2024-06-02 15:22:33 +0700

    da hoan thanh lab10

---
+++ lab10/lab 10/3.py
@@ -0,0 +1,7 @@
+import package1.so_hoc as t
+a=int(input("Nhập a: "))
+b=int(input("Nhập b: "))
+n=int(input("Nhập n: "))
+print(f"UCLN của {a} và {b} là: ",t.Ucln(a,b))
+print(f"BCNN của {a} và {b} là: ",t.Bcnn(a,b))
+print(f"Tổng các ước của {n} là: ",t.Sum_Divisor(n))
